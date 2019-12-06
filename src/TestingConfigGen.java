e3c2d0ae8fdf0f70b39982ef12800622014566d6
ColbyLe <colbyle97@gmail.com>
2019-12-05 21:17:17 -0800

    Added readme file to Images directory

--- src/TestingConfigGen.java
+++ src/TestingConfigGen.java
@@ -10,7 +10,7 @@ public class TestingConfigGen {
         BufferedWriter out = new BufferedWriter(new FileWriter("weeds_test.txt"));
         BufferedWriter outLines = new BufferedWriter(new FileWriter("weeds_lines.txt"));
         // if this doesn't work, just set numTrain to amount of images in Images directory
-        int numTrain = new File("./Images").list().length;
+        int numTrain = new File("./Images").list().length-1;
         int numVal = (int)(numTrain*0.2);
 
         for(int i=0; i<numVal; i++) {
